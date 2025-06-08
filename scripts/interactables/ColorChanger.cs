39374d6e99e2d61d3a761fa4983f8bfd5afd8309
PixelDough <alworrell19@gmail.com>
2025-06-08 18:10:07 -0500

    set color changer to refer to an instance of GeometryInstance3D to manipulate the per instance material

--- scripts/interactables/ColorChanger.cs
+++ scripts/interactables/ColorChanger.cs
@@ -4,10 +4,18 @@ namespace PointAndClick.Scripts.Interactables;
 
 public partial class ColorChanger : Node
 {
-    [Export] private BaseMaterial3D _material;
+    [Export] private GeometryInstance3D _geometryInstance3D;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        var material = _geometryInstance3D.GetMaterialOverride().Duplicate() as BaseMaterial3D;
+        _geometryInstance3D.SetMaterialOverride(material);
+    }
 
     public void ChangeColor()
     {
-        _material.AlbedoColor = Color.FromHsv(GD.Randf(), 1f, 1f, 1f);
+        ((BaseMaterial3D)_geometryInstance3D.MaterialOverride).AlbedoColor = Color.FromHsv(GD.Randf(), 1f, 1f, 1f);
     }
 }
