69f251ca4bc11c07542cba8ae3977e58e763f0f8
PixelDough <alworrell19@gmail.com>
2025-06-09 00:24:47 -0500

    worked on lighting and materials

--- scripts/interactables/ColorChanger.cs
+++ scripts/interactables/ColorChanger.cs
@@ -6,16 +6,9 @@ public partial class ColorChanger : Node
 {
     [Export] private GeometryInstance3D _geometryInstance3D;
 
-    public override void _Ready()
-    {
-        base._Ready();
-
-        var material = _geometryInstance3D.GetMaterialOverride().Duplicate() as BaseMaterial3D;
-        _geometryInstance3D.SetMaterialOverride(material);
-    }
-
     public void ChangeColor()
     {
-        ((BaseMaterial3D)_geometryInstance3D.MaterialOverride).AlbedoColor = Color.FromHsv(GD.Randf(), 1f, 1f, 1f);
+        var randomColor = Color.FromHsv(GD.Randf(), 1f, 1f, 1f);
+        _geometryInstance3D.SetInstanceShaderParameter("tint", randomColor);
     }
 }
